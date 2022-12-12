fda0d7b5bff61c0aa179b4d87ef9788a07faf3b7
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-12-12 23:48:16 +0000

    add basic register form

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,9 +1,48 @@
 import './Register.scss';
+import { useState } from 'react';
+import axios from 'axios';
+
+type RegisterRequest = {
+  Firstname: string;
+  Lastname: string;
+  Email: string;
+  Password: string;
+}
 
 function Register() {
+
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function registerUser() {
+    await axios.post('http://localhost:3000/users/register', {
+      Firstname: firstname,
+      Lastname: lastname,
+      Email: email,
+      Password: password
+    })
+  }
+
   return (
     <div className="Register">
       <h1>Register</h1>
+      <form className="registerForm">
+        <label htmlFor="firstname">Firstname</label>
+        <input type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
+
+        <label htmlFor="lastname">Lastname</label>
+        <input type="text" value={lastname} onChange={(e) => setLastname(e.target.value)} />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+        <button type="button" onClick={registerUser}>Register</button>
+      </form>
     </div>
   )
 }
