bf1d7b1c3e6a2ec6b25b04715cb8b9a571941c14
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-12-12 23:21:09 +0000

    create Register component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.scss';
+import Register from './components/Register/Register';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
@@ -8,10 +9,10 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
-
+        <Register />
       </div>
     </QueryClientProvider>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Register/Register.tsx
@@ -0,0 +1,11 @@
+import './Register.scss';
+
+function Register() {
+  return (
+    <div className="Register">
+      <h1>Register</h1>
+    </div>
+  )
+}
+
+export default Register;
