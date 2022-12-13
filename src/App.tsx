d77246c3ff39232ab00baaca0f2261bdaeed871a
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-12-13 12:22:38 +0000

    button for toggling forms

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,27 @@ import './App.scss';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { useState } from 'react';
 
 const queryClient = new QueryClient();
 
 function App() {
+  
+  const [form, setForm] = useState('login');
+
+  function handleFormChange() {
+    if (form === 'login') {
+      setForm('register');
+    } else {
+      setForm('login');
+    }
+  }
 
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
-        <Register />
-        <Login />
+        { form === 'login' ? <Login /> : <Register /> }
+        { form === 'login' ? <button onClick={handleFormChange}>Don't have an account?</button> : <button onClick={handleFormChange}>Already have an account?</button> }
       </div>
     </QueryClientProvider>
   )
