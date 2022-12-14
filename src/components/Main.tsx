b4fc546659c89d313f374c73b5795f140147b59f
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-12-14 23:59:47 +0000

    move handleLogout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,22 +1,24 @@
 import axios from "axios";
 
 function Main() {
-  async function handleLogout() {
-    const res = await axios.get('http://localhost:3000/auth/logout', {
-      withCredentials: true,
-    })
-    if (res && res.status === 200) {
-      sessionStorage.removeItem('user');
-      window.location.reload();
-    }
-  }
-
   return (
     <div>
       <h1>Main</h1>
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
+}
+
+async function handleLogout() {
+  const res = await axios.get('http://localhost:3000/auth/logout', {
+    withCredentials: true,
+    }
+  )
+  
+  if (res && res.status === 200) {
+    sessionStorage.removeItem('user');
+    window.location.reload();
+  }
 }
 
 export default Main;
