8d4bca4e242861464dc27615afccd45ca866df37
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-12-17 11:53:22 +0000

    create timer component

--- src/components/Pomodoro/Pomodoro.tsx
+++ src/components/Pomodoro/Pomodoro.tsx
@@ -1,7 +1,12 @@
+import Timer from './Timer/Timer';
+
 function Pomodoro() {
+  // TODO: Request workTime and breakTime from the user
+
   return (
     <div className="pomodoro">
       <h1>Pomodoro</h1>
+      <Timer />
     </div>
   )
 }
---
+++ src/components/Pomodoro/Timer/Timer.tsx
@@ -0,0 +1,11 @@
+import './Timer.scss';
+
+function Timer() {
+  return (
+    <div className="timer">
+      <h1>Timer</h1>
+    </div>
+  )
+}
+
+export default Timer;
