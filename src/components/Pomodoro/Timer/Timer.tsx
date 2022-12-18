a740abcd5dc1dcf7f9aff526aeed9b056f074dd4
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-12-18 12:24:37 +0000

    countdown timer

--- src/components/Pomodoro/Timer/Timer.tsx
+++ src/components/Pomodoro/Timer/Timer.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
 import './Timer.scss';
 
 function Timer() {
+  const [seconds, setSeconds] = useState(3);
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setSeconds(seconds - 1);
+    }, 1000);
+
+    return () => clearInterval(countdown);
+  }, [seconds]);
+
   return (
     <div className="timer">
-      <h1>Timer</h1>
+      <h1>{seconds}</h1>
     </div>
   )
 }
