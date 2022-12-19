f0fa28468839d01a7dead9a9392faa9495d7f342
Bryan McCarthy <bryanmccarthy2@gmail.com>
2022-12-19 17:14:08 +0000

    settings wip

--- src/components/Pomodoro/Pomodoro.tsx
+++ src/components/Pomodoro/Pomodoro.tsx
@@ -1,5 +1,4 @@
 import './Pomodoro.scss'
-import Timer from './Timer/Timer';
 import Settings from './Settings/Settings';
 import { useState } from 'react';
 
@@ -9,9 +8,18 @@ interface PomodoroProps {
 }
 
 function Pomodoro({ showPomodoro, setShowPomodoro }: PomodoroProps) {
-  const [currentTimer, setCurrentTimer] = useState('');
-  const [workDuration, setWorkDuration] = useState(30);
-  const [breakDuration, setBreakDuration] = useState(30);
+  const [currentTimer, setCurrentTimer] = useState('work');
+  const [seconds, setSeconds] = useState(0);
+  const [workDuration, setWorkDuration] = useState(25 * 60);
+  const [breakDuration, setBreakDuration] = useState(5 * 60);
+  const [formattedDuration, setFormattedDuration] = useState('25:00');
+  const [isActive, setIsActive] = useState(false);
+
+  console.log('currentTimer: ', currentTimer);
+  console.log('seconds', seconds);
+  console.log('workDuration', workDuration);
+  console.log('breakDuration', breakDuration);
+  console.log('isActive', isActive);
 
   // TODO: Request workDuration and breakDuration from the user
 
@@ -19,11 +27,24 @@ function Pomodoro({ showPomodoro, setShowPomodoro }: PomodoroProps) {
     setShowPomodoro(false);
   }
 
+  const formatDuration = (time: number) => {
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    setFormattedDuration(`${minutes}:${seconds < 10 ? '0' + seconds : seconds}`);
+  }
+
   return (
     <div className="Pomodoro" style={{ visibility: showPomodoro ? "visible" : "hidden" }}>
       <button className="CloseButton" onClick={handleClosePomodoro}>&times;</button>
-      <Timer workDuration={workDuration} breakDuration={breakDuration} currentTimer={currentTimer} />
-      <Settings currentTimer={currentTimer} setCurrentTimer={setCurrentTimer} setWorkDuration={setWorkDuration} setBreakDuration={setBreakDuration} />
+      <div className="Timer">
+        <h1 className="FormattedDuration">{ formattedDuration }</h1>
+      </div>
+      <Settings currentTimer={currentTimer} setCurrentTimer={setCurrentTimer} 
+      setSeconds={setSeconds} workDuration={workDuration} 
+      setWorkDuration={setWorkDuration} breakDuration={breakDuration} 
+      setBreakDuration={setBreakDuration} setFormattedDuration={setFormattedDuration} isActive={isActive} setIsActive={setIsActive} />
     </div>
   )
 }
--- src/components/Pomodoro/Settings/Settings.tsx
+++ src/components/Pomodoro/Settings/Settings.tsx
@@ -3,32 +3,65 @@ import './Settings.scss'
 interface SettingsProps {
   currentTimer: string;
   setCurrentTimer: (timer: string) => void;
+  setSeconds: (seconds: number) => void;
+  workDuration: number;
   setWorkDuration: (duration: number) => void;
+  breakDuration: number;
   setBreakDuration: (duration: number) => void;
+  setFormattedDuration: (duration: string) => void;
+  isActive: boolean;
+  setIsActive: (active: boolean) => void;
 }
 
-function Settings({ currentTimer, setCurrentTimer, setWorkDuration, setBreakDuration }: SettingsProps ) {
+function Settings({ currentTimer, setCurrentTimer, setSeconds, workDuration, setWorkDuration, breakDuration, setBreakDuration, setFormattedDuration, isActive, setIsActive }: SettingsProps ) {
 
   const handleWorkClick = () => {
     setCurrentTimer('work');
+    setSeconds(workDuration);
+    setFormattedDuration(formatDuration(workDuration))
   }
 
   const handleBreakClick = () => {
     setCurrentTimer('break');
+    setSeconds(breakDuration);
+    setFormattedDuration(formatDuration(breakDuration))
   }
 
   const handleWorkDurationChange = () => {
-    setWorkDuration(60 * 60); // TODO: Request workDuration from the user
+    setWorkDuration(workDuration + 100); // TODO: handle change
+    if (currentTimer === 'work') setFormattedDuration(formatDuration(workDuration + 100));
   }
 
   const handleBreakDurationChange = () => {
-    setBreakDuration(5 * 60); // TODO: Request breakDuration from the user
+    setBreakDuration(breakDuration + 100); // TODO: handle change
+    if (currentTimer === 'break') setFormattedDuration(formatDuration(breakDuration + 100));
+  }
+
+  const handleStartStopClick = () => {
+    setIsActive(!isActive);
+  }
+
+  const formatDuration = (time: number) => {
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    return`${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
   }
 
   return (
     <div className="Settings">
-      <button className="Button" style={{backgroundColor: currentTimer === 'work' ? '#000' : '#fff', color: currentTimer === 'work' ? '#fff' : '#000'}} onClick={handleWorkClick}>work</button>
-      <button className="Button" style={{backgroundColor: currentTimer === 'break' ? '#000' : '#fff', color: currentTimer === 'break' ? '#fff' : '#000'}} onClick={handleBreakClick}>break</button>
+      <div className="TimerSettings">
+        <button className="Button" style={{backgroundColor: currentTimer === 'work' ? '#000' : '#fff', color: currentTimer === 'work' ? '#fff' : '#000'}} onClick={handleWorkClick}>work</button>
+        <button className="Button" style={{backgroundColor: currentTimer === 'break' ? '#000' : '#fff', color: currentTimer === 'break' ? '#fff' : '#000'}} onClick={handleBreakClick}>break</button>
+      </div>
+      <div className="DurationSettings">
+        <button className="Button" style={{border: '1px solid black'}} onClick={handleWorkDurationChange}>Change Work</button>
+        <button className="Button" style={{border: '1px solid black'}} onClick={handleBreakDurationChange}>Change Break</button>
+      </div>
+      <div className="StartStopSettings">
+        <button className="Button" onClick={handleStartStopClick}>{ isActive ? 'Stop' : 'Start'}</button>
+      </div>
     </div>
   )
 }
