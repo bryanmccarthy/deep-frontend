cf22bd73151f5c8d5af7b4f4d3601a1e667a0cff
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-01-17 16:31:09 +0000

    notes

--- src/components/Journal/Journal.tsx
+++ src/components/Journal/Journal.tsx
@@ -5,7 +5,18 @@ function Journal() {
   // TODO: deep work journal
   return (
     <div className="Journal">
-      <h1>Journal</h1> 
+      <h1>
+        Different Views:
+      </h1>
+      <h1>
+        Calendar
+      </h1> 
+      <h1>
+        Tasks View: Task Name, Due date (sorted), Priority, Status, Current, TimeElapsed. Task can be marked as current task? store time spent on task
+      </h1>
+      <h1>
+        Tasks can contain multiple notes: Note Name, Note Content
+      </h1>
     </div>
   )
 }
