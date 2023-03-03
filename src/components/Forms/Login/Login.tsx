711c2bd59acb66d0160e714e308473951c2aba3f
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-03-03 19:03:47 +0000

    error snackbars on forms

--- src/components/Forms/Login/Login.tsx
+++ src/components/Forms/Login/Login.tsx
@@ -1,6 +1,9 @@
 import './Login.scss';
 import { useState } from 'react';
 import axios from 'axios';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 type LoginProps = {
   setUser: (user: string | null) => void;
@@ -9,8 +12,12 @@ type LoginProps = {
 function Login({ setUser }: LoginProps) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorSnackbarOpen, setErrorSnackbarOpen] = useState<boolean>(false);
 
   async function loginUser() {
+    if (email === '') return; // TODO: add error handling
+    if (password === '') return; // TODO: add error handling
+
     const res = await axios.post(import.meta.env.VITE_URL + '/auth/login', {
       Email: email,
       Password: password,
@@ -18,12 +25,18 @@ function Login({ setUser }: LoginProps) {
     {
       withCredentials: true,
     })
-    if (res && res.status === 200) {
+    if (res.status === 200) {
       sessionStorage.setItem('userFirstName', res.data.firstname);
       sessionStorage.setItem('userLastName', res.data.lastname);
       setUser(res.data.firstname)
+    } else {
+      setErrorSnackbarOpen(true);
     }
   }
+
+  function handleSnackbarClose() {
+    setErrorSnackbarOpen(false);
+  };
 
   return (
     <div className="Login">
@@ -32,6 +45,25 @@ function Login({ setUser }: LoginProps) {
         <input className="LoginInput" placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <button className="LoginButton" type="button" onClick={loginUser}>Login</button>
       </form>
+
+      <Snackbar
+        open={errorSnackbarOpen}
+        autoHideDuration={5000}
+        onClose={handleSnackbarClose}
+        message="oops, something went wrong!"
+        action={
+          <div>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleSnackbarClose}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </div>
+        }
+      />
     </div>
   )
 }
--- src/components/Forms/Register/Register.tsx
+++ src/components/Forms/Register/Register.tsx
@@ -1,22 +1,39 @@
 import './Register.scss';
 import { useState } from 'react';
 import axios from 'axios';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 function Register() {
   const [firstname, setFirstname] = useState('');
   const [lastname, setLastname] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorSnackbarOpen, setErrorSnackbarOpen] = useState<boolean>(false);
 
   async function registerUser() {
-    await axios.post(import.meta.env.VITE_URL + '/auth/register', {
+    if (firstname === '') return; // TODO: add error handling
+    if (lastname === '') return; // TODO: add error handling
+    if (email === '') return; // TODO: add error handling
+    if (password === '') return; // TODO: add error handling
+
+    const res = await axios.post(import.meta.env.VITE_URL + '/auth/register', {
       Firstname: firstname,
       Lastname: lastname,
       Email: email,
       Password: password
     })
+
+    if (res.status !== 200) {
+      setErrorSnackbarOpen(true);
+    }
     // TODO: Clear form fields & display success message and switch to login page
   }
+
+  function handleSnackbarClose() {
+    setErrorSnackbarOpen(false);
+  };
 
   return (
     <div className="Register">
@@ -27,6 +44,25 @@ function Register() {
         <input className="RegisterInput" placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <button className="RegisterButton" type="button" onClick={registerUser}>Register</button>
       </form>
+
+      <Snackbar
+        open={errorSnackbarOpen}
+        autoHideDuration={5000}
+        onClose={handleSnackbarClose}
+        message="oops, something went wrong!"
+        action={
+          <div>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleSnackbarClose}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </div>
+        }
+      />
     </div>
   )
 }
