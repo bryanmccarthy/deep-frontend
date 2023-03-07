3351b14e72933430e33dc8413e45cc94d832638e
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-03-07 12:43:08 +0000

    get user total time spent test

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -4,10 +4,7 @@ import axios from 'axios';
 function Dashboard() {
 
   async function makeRequest() {
-    const res = await axios.put(import.meta.env.VITE_URL + '/user/update/time_spent', {
-      time_spent: 600,
-    },
-    {
+    const res = await axios.get(import.meta.env.VITE_URL + '/user/time_spent', {
       withCredentials: true,
     });
 
@@ -24,7 +21,7 @@ function Dashboard() {
       <h1>Dashboard</h1>
       <div className="TEST">
 
-        <button onClick={makeRequest}>Make Request</button>
+        <button onClick={makeRequest}>Get total time spent</button>
 
       </div>
     </div>
