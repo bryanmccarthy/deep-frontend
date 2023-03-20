ed626e86f1db53ec60e23c17b40b7452d34ad7bc
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-03-20 23:34:43 +0000

    hide add task in dashboard

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -120,7 +120,7 @@ function Main() {
           <DashboardIcon className="Icon" onClick={handleDashboardPageClick} style={{ color: page === 'dashboard' ? accent2 : accent }} />
         </div>
         <div>
-          <PlaylistAddIcon className="Icon" onClick={handleShowAddTask} style={{ color: showAddTask ? accent2 : accent, visibility: page === 'expandedTask' ? 'hidden' : 'visible' }} />
+          <PlaylistAddIcon className="Icon" onClick={handleShowAddTask} style={{ color: showAddTask ? accent2 : accent, visibility: page === 'expandedTask' || page === 'dashboard' ? 'hidden' : 'visible' }} />
           <TimerIcon className="Icon" onClick={handleShowPomodoro} style={{ color: showPomodoro ? accent2 : accent }} />
           <LogoutIcon className="Icon" onClick={handleLogout} />
         </div>
